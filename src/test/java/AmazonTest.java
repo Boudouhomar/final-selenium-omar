bb91295b7a76757c8ae8761db2f5fb5e99e1653f
unknown <boudouhomar00@gmail.com>
2023-04-06 14:45:06 +0200

    first commit

--- src/test/java/AmazonTest.java
+++ src/test/java/AmazonTest.java
@@ -1,2 +1,35 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageobjects.HomePage;
+
 public class AmazonTest {
+
+    WebDriver driver;
+
+
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.amazon.fr");
+        driver.manage().window().maximize();
+    }
+
+  /*  @AfterMethod
+    public void teardown() {
+        driver.quit();
+    }*/
+
+    @Test
+    public void testAmazon(){
+
+        HomePage homePage = new HomePage(driver);
+
+
+
+
+    }
+
 }
